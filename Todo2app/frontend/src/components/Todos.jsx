65da2c73a460279d96da125aafde5842320b673f
shubham joshi <shubhamjoshi1902@gmail.com>
2024-07-16 22:26:59 +0530

    alert function , little style update

--- Todo2app/frontend/src/components/Todos.jsx
+++ Todo2app/frontend/src/components/Todos.jsx
@@ -1,13 +1,26 @@
-import { DATE } from "mysql/lib/protocol/constants/types";
+// import { DATE } from "mysql/lib/protocol/constants/types";
 
 export function Todos({ todos }) {
   return todos.map(function (element) {
     return (
-      <div key={element._id} data-id={element._id}>
+      <div
+        style={{
+          border: "5px solid black",
+          margin: "10px",
+          padding: "10px",
+          borderRadius: "10px",
+        }}
+        key={element._id}
+        data-id={element._id}
+      >
         <h1>{element.title}</h1>
         <h2>{element.description}</h2>
         <button
           onClick={async function (e) {
+            if (element.completed) {
+              alert("already completed");
+              return;
+            }
             const id = e.target.closest("div").dataset.id;
             console.log(id);
             await fetch(`http://localhost:3000/completed`, {
@@ -18,6 +31,8 @@ export function Todos({ todos }) {
               body: JSON.stringify({
                 id: id,
               }),
+            }).then(function (res) {
+              alert("updated");
             });
           }}
         >
